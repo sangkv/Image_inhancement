5616dfccb3ba12205fc48adca079f5004f109075
Sangkv <sangkimit@gmail.com>
2020-06-06 10:33:18 +0700

    Add images

--- Histogram_Equalization_HSV.py
+++ Histogram_Equalization_HSV.py
@@ -9,8 +9,8 @@
 import cv2
 #import matplotlib.pyplot as plt 
 
-
-im = cv2.imread("airblade.jpeg")
+name = "IMG_0805"
+im = cv2.imread(name + ".JPG")
 
 #(b, r, g) = cv2.split(im)
 
@@ -26,8 +26,9 @@ hsv_equ = cv2.merge((h, s, equ))
 
 img_output = cv2.cvtColor(hsv_equ, cv2.COLOR_HSV2BGR)
 
-cv2.imshow("View",img_output)
-cv2.imwrite("airblade_equalization.jpg", img_output)
+cv2.namedWindow('View Output', cv2.WINDOW_NORMAL)
+cv2.imshow("View Output",img_output)
+cv2.imwrite(name + "_output" + ".jpg", img_output)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
